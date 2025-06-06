77aaa04736757d2d24124cf59aeeb99e7f97e77f
Zoha-Khan123 <zohakhan11004@gmail.com>
2025-06-06 16:16:39 +0500

    responsive correct

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useState } from "react";
 import { CiHeart } from "react-icons/ci";
@@ -9,16 +9,13 @@ import { MdKeyboardArrowDown, MdOutlineAccountCircle } from "react-icons/md";
 import { PiShoppingCart } from "react-icons/pi";
 
 const Navbar = () => {
-  const [toggle,setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
     <>
-      {/* topbar */}
-      <div className="topbar">
-        <p>
-          Carry Confidence with Zohrif Wallets Free Shipping + 50% Off for a
-          Limited Time
-        </p>
-        <select className="selectbox">
+      {/* Top-bar */}
+      <div className="top-bar">
+        <p>Carry Confidence with Zohrif Wallets Free Shipping + 50% Off for a Limited Time </p>
+        <select className="select-box">
           <option>English</option>
           <option>Urdu</option>
         </select>
@@ -26,36 +23,38 @@ const Navbar = () => {
 
       {/* Navbar */}
       <div className="navbar">
-        <div><h1 className="brandName">Zohrif</h1></div>
+        <div><h1 className="brand-name">Zohrif</h1></div>
 
-        <ul className="navLinks">
+        <ul className="nav-links">
           <li><Link href="/home">Home</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/signup">Signup</Link></li>
-          <li className="categories">Categories <span><MdKeyboardArrowDown /></span></li>
+          <li className="categories">Categories<span><MdKeyboardArrowDown /></span></li>
         </ul>
 
-      <div className="searchboxAndIcons">
-        <div className="inputbox"><input type="text" placeholder="What are you looking for?"/><span><IoSearchOutline /></span></div>
-        <div className="icons"><CiHeart /><PiShoppingCart /><div><MdOutlineAccountCircle /></div></div>
-      </div>
-      {
-       toggle ? <IoMdClose className="close" onClick={()=>setToggle(false)}/> : <FaBars className="on" onClick={()=>setToggle(true)}/>
-      }
+        {/* Search box and icons */}
+        <div className="searchbox-and-icons">
+          <div className="input-box"><input type="text" placeholder="What are you looking for?" /><span> <IoSearchOutline /></span></div>
+          <div className="icons">
+          <CiHeart /><PiShoppingCart />
+          <div><MdOutlineAccountCircle /></div>
+          </div>
+        </div>
+
+        {/* Icon wrapper */}
+        <div className="icon-wrapper">
+          {toggle ? (<IoMdClose onClick={() => setToggle(false)} />) : (<FaBars onClick={() => setToggle(true)} />)}
+        </div>
+
       </div>
 
-              
       {/* Responsive */}
-        <ul className={`responsiveCategory ${toggle ? 'right-0' : 'right-[-100%]'}`}>
-          <li>Home</li>
-          <li>About</li>
-          <li>Sign Up</li>
-          <li className="categories">Categories <span><MdKeyboardArrowDown /></span></li>
-        </ul>
-
-
-
-
+      <ul className={`responsive-category ${toggle ? "right-0" : "right-[-100%]"}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Sign Up</li>
+        <li className="categories">Categories <span> <MdKeyboardArrowDown /></span></li>
+      </ul>
     </>
   );
 };
